83d595e89abbdc85a616610747e5bc9a5f688ac7
Dania Darweesh <daniadarweesh.d@gmail.com>
2022-01-27 13:47:13 -0800

    item.js

--- src/App.js
+++ src/App.js
@@ -2,23 +2,31 @@ import './App.css';
 import Login from './components/userAuthorization/Login';
 import Register from './components/userAuthorization/Register';
 import Home from './components/Home';
-import {Route,Routes,BrowserRouter as Router} from 'react-router-dom'
+import  Item from './components/Item';
+import {Route,Routes,BrowserRouter as Router, useParams} from 'react-router-dom'
+
+
+
+
 
 function App() {
+ 
+  
   return (
     <div className="App">
-      <Router>
+     
+     <Router>
         /**put navbar here */
         <Routes>
         <Route exact path="/login" element={ <Login/>}/>
         <Route exact path="/register" element={ <Register/>}/>
         <Route exact path="/" element={ <Home/>}/>
+        <Route exact path="/item" element={ <Item/>}/>
         
      
      
       </Routes>
       </Router>
-    
     </div>
   );
 }
