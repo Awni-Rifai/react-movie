d4fad7d27e94b014b3a97289d4e63158414caf48
Ali-Obeidat <ali.hus.obeidat@gamil.com>
2022-01-30 15:28:42 +0200

    comments

--- src/components/single-product/Content.js
+++ src/components/single-product/Content.js
@@ -3,27 +3,95 @@ import Comments from './Comments';
 import Gallery from './Gallery';
 import Related_movie from './Related_movie';
 import Reviews from './Reviews';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom'
 
-export default class Content extends Component {
+import { db } from '../userAuthorization/firebase'
+import { collection, addDoc, Timestamp, query, onSnapshot, where, getDocs } from 'firebase/firestore'
+
+function Content(props) {
+    // const [body, setBody] = useState("");
+    const [comments, setComments] = useState([])
+    const [commentId, setCommentId] = useState(null)
+    // let comment_id = ""
+    let { id, movie_id } = useParams();
     
-    render() {
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-12 col-lg-8 col-xl-8">
-                        {/* <!-- content tabs --> */}
-                        <div className="tab-content" id="myTabContent">
-                            {this.props.commentsState && <Comments />}
-                            {this.props.reviewsState &&  <Reviews />}
-                            {this.props.galleryState &&  <Gallery/>}
-                        </div>
-                        {/* <!-- end content tabs --> */}
-                    </div>
-                    {/* <!-- sidebar --> */}
-                    <Related_movie />
-                    {/* <!-- end sidebar --> */}
-                </div>
-            </div>
-        );
+   const addComment = async (body)=>{
+
+    console.log("hello",body);
+    const doc = { body: body, user_id: 0, movie_id: props.movieInfo.id, created: Timestamp.now() }
+    console.log(doc);
+    try {
+         addDoc(collection(db, 'comments'),doc).then(() =>{
+             
+            setComments([...comments,comments])
+        })
+        console.log("comment created");
+    } catch (err) {
+        console.log(err)
     }
+   }
+            useEffect(()=>{
+                console.log("useEffect");
+                if (id) {
+                    fetchComments(id)
+                } else if (movie_id) {
+                    fetchComments(movie_id)
+
+                }
+            },[id,movie_id])
+    
+    console.log(commentId);
+
+
+    const handleSubmit = async (e) => {
+       
+    }
+    const fetchComments = (id)=>{
+        const q = query(collection(db, 'comments'), where('movie_id', "==", Number(id)));
+
+        onSnapshot(q, (querySnapshot) => {
+            console.log(querySnapshot);
+            if (querySnapshot.empty) {
+                console.log("empty");
+            }
+            console.log(querySnapshot);
+              let  results=[]
+            querySnapshot.docs.forEach((doc) => {
+                setComments()
+                console.log('doc',doc.data());
+                results.push({ ...doc.data(), id: doc.id })
+            })
+            setComments(results)
+        })
+    }
+
+    
+
+    console.log('comments',comments);
+
+
+
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-12 col-lg-8 col-xl-8">
+                    {/* <!-- content tabs --> */}
+                    <div className="tab-content" id="myTabContent">
+                        {props.commentsState && <Comments  addComment={addComment} handleSubmit={handleSubmit}  comments={comments} />}
+                        {props.reviewsState && <Reviews />}
+                        {props.galleryState && <Gallery />}
+                    </div>
+                    {/* <!-- end content tabs --> */}
+                </div>
+                {/* <!-- sidebar --> */}
+                <Related_movie />
+                {/* <!-- end sidebar --> */}
+            </div>
+        </div>
+    );
+
 }
+
+export default Content
--- src/components/userAuthorization/firebase.js
+++ src/components/userAuthorization/firebase.js
@@ -9,17 +9,27 @@ import { getAuth} from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDu7-8QeorOw814y9cqFS_1s3vwJPSs87E",
-  authDomain: "react-movie-e3147.firebaseapp.com",
-  projectId: "react-movie-e3147",
-  storageBucket: "react-movie-e3147.appspot.com",
-  messagingSenderId: "42861137742",
-  appId: "1:42861137742:web:5ad8ec95a9f57292abd81f",
-  measurementId: "G-09D8F3C8GG"
+  apiKey: "AIzaSyCSpCG2T_yhZBY-LGN-d0vpbbinK0TF0wk",
+  authDomain: "ali-fire-test.firebaseapp.com",
+  projectId: "ali-fire-test",
+  storageBucket: "ali-fire-test.appspot.com",
+  messagingSenderId: "32079689380",
+  appId: "1:32079689380:web:d7feff7c7822e25fe11831"
 };
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDu7-8QeorOw814y9cqFS_1s3vwJPSs87E",
+//   authDomain: "react-movie-e3147.firebaseapp.com",
+//   projectId: "react-movie-e3147",
+//   storageBucket: "react-movie-e3147.appspot.com",
+//   messagingSenderId: "42861137742",
+//   appId: "1:42861137742:web:5ad8ec95a9f57292abd81f",
+//   measurementId: "G-09D8F3C8GG"
+// };
 
 // Initialize Firebase
+
  const firebaseApp = initializeApp(firebaseConfig);
  const db = getFirestore();
  const auth=getAuth();
